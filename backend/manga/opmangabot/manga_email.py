63b9da1380daad6f876c272bbf0e6c7d858edf76
AnilSahintuerk <sahintuerk.anil@gmail.com>
2022-03-22 00:29:59 +0100

    mail push

--- backend/manga/opmangabot/manga_email.py
+++ backend/manga/opmangabot/manga_email.py
@@ -8,20 +8,15 @@ import os
 import shutil
 
 
-def main(chapter):
+def main(email, chapter):
+
     sender = 'onepiece.manga.bot@gmail.com'
     password = 'Trafalgarlaw'
     body = 'Viel spaß beim lesen'
 
-    receivers = []
 
     path_parent = os.path.dirname(os.getcwd())
     os.chdir(path_parent)
-
-    with open(os.path.join('opmangabot', 'email.txt')) as f:
-        email_txt = csv.reader(f)
-        for row in email_txt:
-            receivers.append(row)
 
     cwd = os.getcwd()
     folder = "One_Piece_Manga"
@@ -32,49 +27,41 @@ def main(chapter):
     else:
         os.chdir(cwd)
 
-    # pdfs = [pdf for pdf in os.listdir()]
-    # pdfs.sort()
-    # pdfs = pdfs[-1]
-    # print(pdfs)
 
-    pdfs = [str(chapter) + '.pdf']
+    pdf = str(chapter) + '.pdf'
 
-    for email in receivers:
-        email = email[0]
+   
+    # Setup the MIME
+    message = MIMEMultipart()
+    message['From'] = sender
+    message['To'] = email
+    message['Subject'] = 'Chapter ' + pdf.split('.')[0]
 
-        for pdf in pdfs:
-            # Setup the MIME
-            message = MIMEMultipart()
-            message['From'] = sender
-            message['To'] = email
-            message['Subject'] = 'Chapter ' + pdf.split('.')[0]
+    message.attach(MIMEText(body, 'plain'))
 
-            message.attach(MIMEText(body, 'plain'))
+    # open the file in binary
+    binary_pdf = open(pdf, 'rb')
+    payload = MIMEBase('application', 'octate-stream', Name=pdf)
+    payload.set_payload((binary_pdf).read())
 
-            # open the file in binary
-            binary_pdf = open(pdf, 'rb')
-            payload = MIMEBase('application', 'octate-stream', Name=pdf)
-            payload.set_payload((binary_pdf).read())
+    # enconding the binary into base64
+    encoders.encode_base64(payload)
 
-            # enconding the binary into base64
-            encoders.encode_base64(payload)
+    # add header with pdf name
+    payload.add_header('Content-Decomposition',
+                        'attachment', filename=pdf)
+    message.attach(payload)
 
-            # add header with pdf name
-            payload.add_header('Content-Decomposition',
-                               'attachment', filename=pdf)
-            message.attach(payload)
+    # use gmail with port
+    session = smtplib.SMTP('smtp.gmail.com', 587)
 
-            # use gmail with port
-            session = smtplib.SMTP('smtp.gmail.com', 587)
+    # enable security
+    session.starttls()
 
-            # enable security
-            session.starttls()
+    # login with mail_id and password
+    session.login(sender, password)
 
-            # login with mail_id and password
-            session.login(sender, password)
+    text = message.as_string()
+    session.sendmail(sender, email, text)
+    session.quit()
 
-            text = message.as_string()
-            session.sendmail(sender, email, text)
-            session.quit()
-
-    # shutil.rmtree(cwd)
